8631d459ed2584094ce178aadbc9ffd4b0cff226
dev au <devmobile@devmobile.local>
2022-05-12 10:37:40 +0700

    change url

--- authentication/authentication_with_api/api/apiUrl.dart
+++ authentication/authentication_with_api/api/apiUrl.dart
@@ -4,10 +4,10 @@
 // final String baseUrl = MyPluginAppEnvironment().baseUrl!;
 
 // class APIUrl {
-//   static String getStarted = '$baseUrl$versionApi/auth/get-started/';
-//   static String getProfile = '$baseUrl$versionApi/auth/profile/';
-//   static String fcm = '$baseUrl$versionApi/auth/fcm-device/';
-//   static String upload = '$baseUrl$versionApi/upload/';
+// static String getStarted = '${baseUrl}general/auth/get-started/';
+// static String getProfile = '${baseUrl}general/auth/profile/';
+// static String fcm = '${baseUrl}general/auth/fcm-device/';
+// static String upload = '${baseUrl}general/auth/upload/';
 
 //   //NOTIFICATION
 //   static final listNotification = '$baseUrl$versionApi/user/notifications/';
@@ -16,12 +16,12 @@
 //   static final readANotification =
 //       '$baseUrl$versionApi/user/notifications/read/{notification_id}/';
 
-//   static String login = '$baseUrl$versionApi/auth/login/';
-//   static String refreshToken = '$baseUrl$versionApi/auth/refresh-token/';
-//   static String signUp = '$baseUrl$versionApi/auth/signup/';
-//   static String verify = '$baseUrl$versionApi/auth/verify/';
-//   static String resendCode = '$baseUrl$versionApi/auth/resend-code/';
-//   static String resendPassword = '$baseUrl$versionApi/auth/resend-password/';
-//   static String resetPassword = '$baseUrl$versionApi/auth/reset-password/';
-//   static String updatePassword = '$baseUrl$versionApi/auth/update-password/';
+//   static String login = '${baseUrl}general/auth/login/';
+//   static String refreshToken = '${baseUrl}general/auth/refresh-token/';
+//   static String signUp = '${baseUrl}general/auth/complete-signup/';
+//   static String verify = '${baseUrl}general/auth/verify/';
+//   static String resendCode = '${baseUrl}general/auth/resend-code/';
+//   static String resendPassword = '${baseUrl}general/auth/resend-password/';
+//   static String resetPassword = '${baseUrl}general/auth/reset-password/';
+//   static String updatePassword = '${baseUrl}general/auth/update-password/';
 // }
