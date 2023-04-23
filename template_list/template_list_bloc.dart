6727899b26ef8f92e44453bb17a7b84a05d9af7f
dev au <>
2023-04-23 15:15:33 +0700

    optimise code

--- template_list/template_list_bloc.dart
+++ template_list/template_list_bloc.dart
@@ -34,7 +34,7 @@ class TemplateListBloc extends Bloc<TemplateListEvent, TemplateListState> {
               isLoading: !event.isLoadingMore && !event.isFreshing,
               isRefreshing: !event.isLoadingMore && event.isFreshing)));
       ListModel<TemplateListModel> newTemplateListList =
-          await templateListRepositories.getListTemplateList(
+          await templateListRepositories.getTemplateList(
               url: event.isLoadingMore
                   ? state.listTemplateList.next!
                   : APIUrl.listTemplateList);
